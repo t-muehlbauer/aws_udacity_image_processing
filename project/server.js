9980d3bf9b93a1ddb2065120f63a8ea0744628fc
t.muehlbauer <t.muehlbauer@hm.edu>
2024-09-06 13:23:42 +0200

    implemented GET

--- project/server.js
+++ project/server.js
@@ -30,6 +30,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
     /**************************************************************************** */
 
   //! END @TODO1
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  app.get('/filteredimage', async (req, res) => {
+    const { image_url } = req.query;
+
+    // 1. Validate the image_url query
+    if (!image_url) {
+      return res.status(400).send({ message: 'image_url query parameter is required.' });
+    }
+
+    try {
+      // 2. Call filterImageFromURL(image_url) to filter the image
+      const filteredPath = await filterImageFromURL(image_url);
+
+      // 3. Send the resulting file in the response
+      res.sendFile(filteredPath, (err) => {
+        if (err) {
+          return res.status(500).send({ message: 'Failed to send the filtered image.' });
+        }
+
+        // 4. Delete any files on the server on finish of the response
+        deleteLocalFiles([filteredPath]);
+      });
+    } catch (error) {
+      res.status(422).send({ message: 'Unable to process the image at the provided URL.' });
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
